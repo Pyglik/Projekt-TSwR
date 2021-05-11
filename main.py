e4d4249eed27f56d1f7d90357cc1d226a7672cca
Pyglik <pyglik@o2.pl>
2021-05-12 01:48:17 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -12,7 +12,19 @@ class MobileRobot(StateMachine):
     stop = przod.to(bezczynny)
     sk_lewo = przod.to(lewo)
     sk_prawo = przod.to(prawo)
-    koniec_sk_l = sk_lewo.to(przod)
-    koniec_sk_p = sk_prawo.to(przod)
+    koniec_sk_l = lewo.to(przod)
+    koniec_sk_p = prawo.to(przod)
     przeszkoda = przod.to(omijanie)
     koniec_om = omijanie.to(przod)
+
+
+robot = MobileRobot()
+print(robot.current_state)
+print(robot.current_state == robot.bezczynny)
+print(robot.is_bezczynny)
+
+print([s.identifier for s in robot.states])
+print([t.identifier for t in robot.transitions])
+
+robot.start()
+print(robot.current_state)
